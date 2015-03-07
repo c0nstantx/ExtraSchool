3f275207dc787dad2caf6f9620660b6f9fbd6a66
Konstantinos Christofilos <k.christofilos@intelen.com>
2015-03-07 11:45:23 +0200

    Authenticator tests

---
+++ test/models/util/AuthenticatorTest.java
@@ -0,0 +1,30 @@
+package models.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import models.domain.EntityBaseTest;
+import models.domain.User;
+import controllers.service.UserService;
+
+/**
+ * 
+ * @author Konstantinos Christofilos <kostasxx@gmail.com>
+ * @author Pavlos Gerardos <pavlos.g@gmail.com >
+ * @author Sokratis Pantazaras <spantazaras@gmail.com>
+ *
+ */
+public class AuthenticatorTest extends EntityBaseTest
+{
+	@Test
+	public void authenticate()
+	{
+		Authenticator auth = new Authenticator();
+		UserService us = new UserService();
+		User user = us.findUserByUsername("kchristofilos");
+		
+		Assert.assertNull(auth.authenticate("kchristofilos", "123456789"));
+		Assert.assertNull(auth.authenticate("kostasx", "123456"));
+		Assert.assertEquals(user, auth.authenticate("kchristofilos", "123456"));
+	}
+}
