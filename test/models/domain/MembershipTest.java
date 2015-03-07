72e51efe61455a135889f53b19868c0b09015cbb
Konstantinos Christofilos <k.christofilos@intelen.com>
2015-03-07 12:08:29 +0200

    Membership entity tests

---
+++ test/models/domain/MembershipTest.java
@@ -0,0 +1,65 @@
+package models.domain;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import models.util.DateLib;
+import controllers.service.ActivityService;
+import controllers.service.UserService;
+
+/**
+ * 
+ * @author Konstantinos Christofilos <kostasxx@gmail.com>
+ * @author Pavlos Gerardos <pavlos.g@gmail.com >
+ * @author Sokratis Pantazaras <spantazaras@gmail.com>
+ *
+ */
+public class MembershipTest extends EntityBaseTest
+{
+
+	@Test
+	public void membershipReport()
+	{
+		ActivityService as = new ActivityService();
+		UserService us = new UserService();
+		
+		Activity activity = as.findByName("activity1");
+		User user = us.findUserByUsername("sok");
+		Membership membership = new Membership();
+		membership.setActivity(activity);
+		membership.setUser(user);
+		membership.setRegistrationDate(DateLib.getDateObject(3, 3, 2015));
+		
+		Report report = new Report();
+		report.setComment("Fair enough");
+		report.setGrade(9.5);
+		report.setPublicationDate(new Date());
+		
+		membership.setReport(report);
+		
+		em.persist(membership);
+		
+		Assert.assertEquals(9.5, membership.getReport().getGrade(), 0.001);
+	}
+
+	@Test
+	public void membershipString()
+	{
+		ActivityService as = new ActivityService();
+		List<Activity> activities = as.findAll();
+		
+		for (Activity activity : activities) {
+			Set<Membership> memberships = activity.getMemberships();
+			for (Membership membership : memberships) {
+				String membershipString = "Membership: "+activity.getId().toString() 
+						+ ", "+membership.getUser().getId().toString() 
+						+ ", "+DateLib.dateAsString(membership.getRegistrationDate());
+				Assert.assertEquals(membershipString, membership.toString());
+			}
+		}
+	}
+}
