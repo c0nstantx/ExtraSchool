2c90a808ccd6ff8c3fdeac4b8e8c86f4d4386ab5
Soookkk <spantazaras@gmail.com>
2015-03-16 15:29:29 +0200

    Changes to DBCreator (not complete yet)

--- app/models/util/DBCreator.java
+++ app/models/util/DBCreator.java
@@ -1,8 +1,6 @@
 package models.util;
 
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Random;
 
 import models.domain.UserType;
@@ -33,71 +31,74 @@ public class DBCreator {
 	
 	private static final int MaximumUniqueNames = FirstNames.length * LastNames.length;
 
-	private static final String Field_NumberOfActivities = "numberOfActivities";
-	private static final int NumberOfActivities = 10;
-	private static final String Field_NumberOfAdmins = "numberOfActivities";
+	private static final int NumberOfActivities = 30;
 	private static final int NumberOfAdmins = 2;
-	private static final String Field_NumberOfTutors = "numberOfTutors";
-	private static final int NumberOfTutors = 10;
-	private static final String Field_NumberOfPupils = "numberOfPupils";
-	private static final int NumberOfPupils = 200;
-	private static final String Field_MinimumAgeInYearsForStaff = "minimumAgeInYearsForStaff";
+	private static final int NumberOfTutors = 30;
+	private static final int NumberOfPupils = 400;
 	private static final int MinimumAgeInYearsForStaff = 25;
-	private static final String Field_MaximumAgeInYearsForStaff = "maximumAgeInYearsForStaff";
 	private static final int MaximumAgeInYearsForStaff = 55;
-	private static final String Field_MinimumAgeInYearsForPupils = "minimumAgeInYearsForPupils";
 	private static final int MinimumAgeInYearsForPupils = 15;
-	private static final String Field_MaximumAgeInYearsForPupils = "maximumAgeInYearsForPupils";
 	private static final int MaximumAgeInYearsForPupils = 18;
 	
 	private static final int SecondsPerYear = 3600 * 24 * 365;
 	private static final int MaxUsernameLength = 5;
 	
-	@SuppressWarnings("rawtypes")
-	private Map parametersMap = new HashMap<String, Integer>();
 	private Random randomGenerator = new Random();
 	private int lastCombinationUsed = 0;
 	
-	public DBCreator() {
-		createParametersMap();
-	}
+	public DBCreator() { }
 	
-	@SuppressWarnings("unchecked")
-	private void createParametersMap() {
-		parametersMap.put(Field_NumberOfActivities, NumberOfActivities);
-		parametersMap.put(Field_NumberOfAdmins, NumberOfAdmins);
-		parametersMap.put(Field_NumberOfTutors, NumberOfTutors);
-		parametersMap.put(Field_NumberOfPupils, NumberOfPupils);
-		parametersMap.put(Field_MaximumAgeInYearsForStaff, MaximumAgeInYearsForStaff);
-		parametersMap.put(Field_MinimumAgeInYearsForStaff, MinimumAgeInYearsForStaff);
-		parametersMap.put(Field_MaximumAgeInYearsForPupils, MaximumAgeInYearsForPupils);
-		parametersMap.put(Field_MinimumAgeInYearsForPupils, MinimumAgeInYearsForPupils);
-	}
-	
-	@SuppressWarnings("unchecked")
-	public void setParameter(String parameterName, int parameterValue) {
-		try {
-			validateParameters(parameterName, parameterValue);
-			parametersMap.put(parameterName, parameterValue);
-		}
-		catch (Exception e) {
-		}
-	}
-	
-	private void validateParameters(String changedParameterName, int changedParameterValue) {
+	public void createStandardData() {
 		
 	}
 	
-	public void createUsers(int numberOfUsers, UserType userType) {
+	public void createRandomData() {
+		
+	}
+	
+	private void createStandardAdmins() {
+		
+	}
+	
+	private void createRandomAdmins() {
+		
+	}
+	
+	private void createStandardTutors() {
+		
+	}
+	
+	private void createRandomTutors() {
+		
+	}
+	
+	private void createStandardPupils() {
+		
+	}
+	
+	private void createRandomPupils() {
+		
+	}
+	
+	private void createStandardUsers(int numberOfUsers, UserType userType) {
+		int[] indices = createSequentialArray(lastCombinationUsed, numberOfUsers);
+		createUsers(indices, userType);
+	}
+	
+	private void createRandomUsers(int numberOfUsers, UserType userType) {
 		int[] indices = createSequentialArray(lastCombinationUsed, numberOfUsers);
 		shuffle(indices);
+		createUsers(indices, userType);
+	}
+	
+	private void createUsers(int[] indices, UserType userType) {
 		for (int i = 0; i < indices.length; i++) {
 			System.out.println(indices[i]);
 		}
-		lastCombinationUsed += numberOfUsers;
+		lastCombinationUsed += indices.length;
 	}
 	
-	private static int[] createSequentialArray(int startIndex, int size) {
+	private int[] createSequentialArray(int startIndex, int size) {
 		int[] seqArray = new int[size];
 		for (int i = 0; i < size; i++) {
 			seqArray[i] = startIndex + i;
@@ -123,11 +124,11 @@ public class DBCreator {
 		return createUsername(firstName, lastName) + "123";
 	}
 	
-	public Date createBirthDate(int minimumAgeInYears, int maximumAgeInYears) {
+	private Date createBirthDate(int minimumAgeInYears, int maximumAgeInYears) {
 		return createBirthDate(DateLib.getDateObject(), minimumAgeInYears, maximumAgeInYears);
 	}
 	
-	public Date createBirthDate(Date currentDate, int minimumAgeInYears, int maximumAgeInYears) {
+	private Date createBirthDate(Date currentDate, int minimumAgeInYears, int maximumAgeInYears) {
 		int randomAgeInYears = minimumAgeInYears + randomGenerator.nextInt(maximumAgeInYears - minimumAgeInYears);
 		Date birthDate = DateLib.copyDateObject(currentDate);
 		DateLib.addYears(birthDate, -randomAgeInYears);
@@ -136,6 +137,6 @@ public class DBCreator {
 	}
 	
 	public static void main(String[] args) {
-		new DBCreator().createUsers(100, UserType.Student);
+		
 	}
 }
