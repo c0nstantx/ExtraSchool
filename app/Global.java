fb263148851f3d0717a512e2196ec4fbe68765e4
Soookkk <spantazaras@gmail.com>
2015-03-17 15:52:08 +0200

    Created Global class which calls Initializer Currently in the process of adding new method to Initializer to create presentation db.

---
+++ app/Global.java
@@ -0,0 +1,42 @@
+import java.util.Properties;
+import java.util.Set;
+
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+
+import models.persistence.Initializer;
+
+import org.hibernate.cfg.Configuration;
+import org.hibernate.tool.hbm2ddl.SchemaUpdate;
+import org.reflections.Reflections;
+
+import play.Application;
+import play.GlobalSettings;
+import play.Logger;
+import play.Play;
+import play.db.jpa.JPA;
+
+public class Global extends GlobalSettings {
+
+	@Override
+	public void onStart(Application app) {
+		Logger.info("Application started...");
+		Logger.info("DataLoader: Loading initial data");
+
+		JPA.withTransaction(new play.libs.F.Callback0() {
+			@Override
+			public void invoke() throws Throwable {
+
+				Initializer init = new Initializer();
+				init.clearDB();
+				init.initDB();
+			}
+		});
+	}
+
+	@Override
+	public void onStop(Application app) {
+		Logger.info("Application shutdown...");
+	}
+
+}
