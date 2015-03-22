d4e14590c1596e2b08189d91b8af2deab2771931
Soookkk <spantazaras@gmail.com>
2015-03-22 18:18:39 +0200

    Started SessionRegistrationBank

--- app/models/domain/RegistrationStatus.java
+++ app/models/domain/RegistrationStatus.java
@@ -3,7 +3,8 @@ package models.domain;
 /**
  * Custom enumerated type for registration status
  * @author Konstantinos Christofilos <kostasxx@gmail.com>
- *
+ * @author Pavlos Gerardos <pavlos.g@gmail.com >
+ * @author Sokratis Pantazaras <spantazaras@gmail.com>
  */
 public enum RegistrationStatus {
 	AbsentWithoutPermission,
